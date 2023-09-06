dfef0ba4782460a158aadb38a151565bd617dbb3
GiancarloCroce <giancarlo.croce01@gmail.com>
2023-09-06 15:04:28 +0200

    MixTCRpred v1.0

---
+++ DeepTCRpred.py
@@ -0,0 +1,249 @@
+import pandas as pd
+import numpy as np
+import os
+import scipy
+from sklearn import metrics
+from tqdm import tqdm
+from argparse import ArgumentParser
+from tabulate import tabulate
+import sys
+import pathlib
+import configparser
+import torch
+import pickle
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader, random_split
+import src.utils
+import src.models
+import src.dataloaders
+import wget
+
+
+#to supprime all warning message and pytorchlighting info
+import warnings
+import logging
+logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
+warnings.filterwarnings('ignore')
+
+
+path_pretrained_models = './pretrained_models'
+
+if __name__ == '__main__':
+
+    parser = ArgumentParser(add_help = False)
+    ###
+    parser.add_argument('-h', '--help', action='store_true') #print help
+    parser.add_argument('--list_models', action='store_true') #print list all models
+    #model params
+    parser.add_argument('-i', '--input', default=None)
+    parser.add_argument('-o', '--output', default=None)
+    parser.add_argument('-m', '--model', default= None)
+    ### for large test set, increase the batch size
+    parser.add_argument('--batch_size', type = int, default= 1)
+    ### to download the model from Zenodo
+    parser.add_argument('--download', default= None)
+    parser.add_argument('--download_all', action = 'store_true')
+    parser.add_argument('--download_high', action = 'store_true')
+    args = parser.parse_args()
+
+    if args.help:
+        print("####################################################################################################")
+        print("# DeepTCRpred v1.0: a sequence-based predictor of TCR-pMHC interaction")
+        print("####################################################################################################")
+        print("Usage: python DeepTCRpred.py --model [DeepTCRpred_model_name] --input [input_file_of_TCRs] --output [output_file]")
+        print("e.g.: python DeepTCRpred.py --model A0201_GILGFVFTL --input ./test/test.csv --output ./test/output.csv")
+        print("----------------------------------------------------------------------------------------------------")
+        print("Arguments:")
+        print("\t[-m] or [--model]. The name of the DeepTCRpred model. 146 pre-trained DeepTCRpred models are currently available" )
+        print("\t[-i] or [--input]. The path to the input .csv file containing the TCR sequences")
+        print("\t[-o] or [--output]. The path to the output file where results will be saved")
+        print("Additional arguments:")
+        print("\t[--list_models]. To visualize the 146 DeepTCRpred models for which we can run predictions. Models with less than 50 training TCRs have low confidence")
+        print("\t[--batch_size]. The default batch size is 1. If you have a large dataset of TCRs to test, increasing the batch_size can speed DeepTCRpred up")
+        print("\t[--download]. To download a specific pretrained DeepTCRpred model (E.g. python DeepTCRpred.py A0201_GILGFVFTL to download the corresponding pretrained DeepTCRpred model)")
+        print("\t[--download_all]. To download the 146 pretrained DeepTCRpred models")
+        print("\t[--download_high]. To download the 43 high-confidence pretrained DeepTCRpred models (with more than 50 training TCRs)")
+        print("\t[-h] or [--help]. To print this help message")
+        sys.exit(0)
+    if args.list_models:
+        import pydoc
+        df  = pd.read_csv(os.path.join(path_pretrained_models, 'info_models.csv'))
+        #print(tabulate(df.drop(columns = ['Peptide', 'AUC_5fold']), headers='keys', tablefmt='psql'))
+        s = "####################################################################################################" + '\n' \
+             + "# DeepTCRpred: a sequence-based predictor of TCR-pMHC interaction" + '\n' \
+            "####################################################################################################" + '\n' \
+            "Usage: python DeepTCRpred.py --model [DeepTCRpred_model_name] --input [input_file_of_TCRs] --output [output_file]" + '\n' \
+            "e.g.: python DeepTCRpred.py --model A0201_GILGFVFTL --input ./test/test.csv --output ./test/output.csv" + '\n' \
+            "" + '\n' \
+            "146 pre-trained DeepTCRpred models are available. Models with less than 50 training TCRs have low confidence." + '\n' \
+            "Use the arrow keys to scroll the list of DeepTCRpred models and press \'q\' to exit" + '\n' \
+            + tabulate(df.drop(columns = ['Peptide', 'AUC_5fold']), headers='keys', tablefmt='psql')
+        pydoc.pager(s)
+        sys.exit(0)
+
+    #to download all the pretrained model
+    if (args.download_all) | (args.download_high):
+        df  = pd.read_csv(os.path.join(path_pretrained_models, 'info_models.csv'))
+        if (args.download_high):
+            min_num_train = 50
+            df  = df.loc[df['Number_training_abTCR'] >= min_num_train]
+            print('Downloading only {0} high-confidence model (at least {1} training abTCR)'.format(len(df), min_num_train))
+        for model_name in df['DeepTCRpred_model_name'].values:
+            if os.path.exists( os.path.join(path_pretrained_models, "model_{0}.ckpt".format(model_name))):
+                print("{0} DeepTCRpred model already downloaded".format(model_name))
+            else:
+                url = "https://zenodo.org/record/7930623/files/model_"+model_name+".ckpt"
+                print("Downloading DeepTCRpred model for {0}".format(model_name))
+                filename = wget.download(url, out = path_pretrained_models)
+        sys.exit(0)
+
+    if args.download != None:
+        print('Downloading TCrpred model for {0})'.format(args.download))
+        url = "https://zenodo.org/record/7930623/files/model_"+args.download+".ckpt"
+        if os.path.exists( os.path.join(path_pretrained_models, "model_{0}.ckpt".format(args.download))):
+            print("{0} DeepTCRpred model already downloaded".format(args.download))
+        else:
+            filename = wget.download(url, out = path_pretrained_models)
+        sys.exit(0)
+
+    #################################################
+    ##########################################for a quick test
+    #args.model = 'A0201_GILGFVFTL'
+    #args.input= './test/test.csv'
+    #args.output = './test/out_test.csv'
+    #########################################################
+
+
+    ##to make input compatible with DeepTCRpred format
+    args.train = None
+    args.test = args.input
+    args.out = args.output
+    args.epitope = args.model.split("_")[-1]
+    args.path_checkpoint = os.path.join(path_pretrained_models, 'model_'+args.model+'.ckpt')
+    args.epochs = 10
+    args.num_workers = 1
+    args.gpus = None
+    args.chain = 'AB'
+    #print(args)
+
+    if os.path.exists(args.test) == False:
+        print("*** Error! Test file not found. Is --test {0} a valid test file? ***".format(args.test))
+        sys.exit(0)
+    if (args.path_checkpoint == None) or (os.path.exists(args.path_checkpoint) == False):
+        print("*** Error! Model not loaded. Is {0} a valid DeepTCRpred model name? For help, run ./DeepTCRpred.py --help ***".format(args.model))
+        sys.exit(0)
+
+    #### determine info epitope
+    df_info  = pd.read_csv(os.path.join(path_pretrained_models, 'info_models.csv'))
+    df_epi_info = df_info.loc[df_info['Peptide'] == args.epitope]
+    model_name = df_epi_info['DeepTCRpred_model_name'].values[0]
+    peptide = df_epi_info['Peptide'].values[0]
+    origin = df_epi_info['Origin'].values[0]
+    MHC_class = df_epi_info['MHC_class'].values[0]
+    MHC = df_epi_info['MHC'].values[0]
+    n_seq = df_epi_info['Number_training_abTCR'].values[0]
+    auc_internal = df_epi_info['AUC_5fold'].values[0]
+    host_species = df_epi_info['Host_species'].values[0]
+    args.host = host_species
+
+    #set config
+    config = configparser.ConfigParser()
+    config['transformer'] = {'padding_epitope':len(args.epitope), 'padding_cdr3_TRA':20, 'padding_cdr3_TRB':20, 'padding_cdr12':10, 'embedding_dim':128, 'hidden_dim':1024, 'num_heads':4, 'num_layers':4, 'dropout':0.2, 'lr':1e-4, 'warmup':1000, 'max_iters': 500, 'num_labels':2}
+    args.padding = [config['transformer'].getint('padding_epitope'), config['transformer'].getint('padding_cdr3_TRA'), config['transformer'].getint('padding_cdr3_TRB'), config['transformer'].getint('padding_cdr12')]
+
+
+    #0.load model
+    data = src.dataloaders.transformer_DataModule(hparams = args)
+
+    model = src.models.TransformerPredictor_AB_cdr123(
+        vocab_size = len(src.utils.valid_aa),
+        embedding_dim = config['transformer'].getint('embedding_dim'),
+        hidden_dim = config['transformer'].getint('hidden_dim'),
+        num_heads = config['transformer'].getint('num_heads'),
+        num_layers = config['transformer'].getint('num_layers'),
+        dropout = float(config['transformer']['dropout']),
+        lr = float(config['transformer']['lr']),
+        num_labels = config['transformer'].getint('num_labels'),
+        #padding
+        padding = args.padding,
+        #for better trainig
+        warmup = config['transformer'].getint('warmup'),
+        max_iters = config['transformer'].getint('max_iters'),
+        )
+
+    #1. Set Trainer
+    path_model_folder = args.path_checkpoint
+    mc = pl.callbacks.ModelCheckpoint(
+            dirpath = path_model_folder,
+            #filename="transformer-{epoch:02d}-{val_loss:.2f}",
+            filename="model_{0}".format(args.epitope), #transformer-{epoch:02d}-{val_loss:.2f}",
+            monitor='val_loss'
+            )
+
+    trainer = pl.Trainer.from_argparse_args(args, default_root_dir = path_model_folder, max_epochs = int(args.epochs), callbacks = [mc], logger = False) #use logger false to disable tensorboard
+
+    #2. Predictions
+    if args.test != None:
+
+        print("#########################################################################")
+        print("###### DeepTCRpred: a sequence-based predictor of TCR-pMHC interaction  ####")
+        print("#########################################################################")
+        print("DeepTCRpred model: {0} ".format(args.model))
+        print("Computing predictions for {0}".format(args.test))
+
+        #reload model from checkpoint
+        model = model.load_from_checkpoint(
+            checkpoint_path= args.path_checkpoint,
+            map_location=None,
+            )
+        trainer.test(model, datamodule = data)
+        test_score = model.prob
+        test_seq = model.test_seq
+        test_tp= model.test_tp
+        #add score model to the test set
+        df_res = data.test.data
+        df_res['score'] = test_score
+
+        #compute perc_rank for a specific epitope
+        l_perc_rank = []
+        d_par = src.utils.d_par
+        for score in df_res['score'].values:
+            pr = src.utils.get_perc_rank(score, args.epitope, d_par)
+            l_perc_rank.append(pr)
+        df_res['perc_rank'] = l_perc_rank
+        df_res = df_res.drop('epitope', axis = 1)
+
+        #sort the results
+        df_res = df_res.sort_values(by = 'score', ascending = False)
+
+        good_model = False
+        if n_seq > 50:
+            good_model = True
+
+        f = open(args.output, 'w')
+        f.write("##############################################################################################\n")
+        f.write("# Output from DeepTCRpred (v1.0)\n")
+        f.write("# Predictions of TCRs binding to {0},{1} - class {2} pMHC  \n".format(MHC, peptide, MHC_class.split("MHC")[-1]))
+        f.write("# Origin: {0}\n".format(origin))
+        f.write("# Input file: {0}\n".format(args.input))
+        f.write("# \n")
+        f.write("# DeepTCRpred is freely available for academic users.\n")
+        f.write("# Private companies should contact Nadette Bulgin](nbulgin@lcr.org) at the Ludwig Institute for Cancer Research Ltd for commercial licenses.\n")
+        f.write("#\n")
+        f.write("# To cite DeepTCRpred,  please refer to: XXXXX\n")
+        f.write("#---------------------------------------------------------------------------\n")
+        f.write("# Binding predictions with DeepTCRpred model {0}\n".format(model_name))
+        if good_model:
+            f.write("# Number of training data {0}. Internal AUC {1:.2f}\n".format(n_seq, auc_internal))
+        else:
+            f.write("# Number of training data {0} (<50!). Low-confidence DeepTCRpred model! \n".format(n_seq))
+        f.write("#---------------------------------------------------------------------------\n")
+        f.write("##############################################################################################\n")
+        f.close()
+
+        print("The results are stored in {0}".format(args.out))
+
+        #round
+        df_res = df_res.round(5)
+
+        df_res.to_csv(args.out, index = False, mode = 'a')
--- README.md
+++ README.md
@@ -1,28 +1,28 @@
-# TCRpred
-TCRpred is a sequence-based TCR-pMHC interaction predictor. 
+# MixTCRpred
+MixTCRpred is a sequence-based TCR-pMHC interaction predictor. 
 TCR binding predictions are currently possible for 146 pMHCs. 
 For 43 pMHC robust predictions were achieved in internal cross validation, while models with less than 50 training TCRs have low confidence.
-[Here](XXXX) the paper describing TCRpred predictive performance and applications.
+[Here](XXXX) the paper describing MixTCRpred predictive performance and applications.
 
 
-## Run TCRpred with GoogleColab
-You can run TCRpred within your web browser via Google Colab by clicking on [this link](https://colab.research.google.com/github/GfellerLab/TCRpred/blob/main/colab_TCRpred.ipynb).
+## Run MixTCRpred with GoogleColab
+You can run MixTCRpred within your web browser via Google Colab by clicking on [this link](https://colab.research.google.com/github/GfellerLab/MixTCRpred/blob/main/colab_MixTCRpred.ipynb).
 This is a user-friendly and interactive way to analyze the specificity of your own TCR list.
-For more extensive analysis or if you prefer to use TCRpred offline, it is recommended to install it on your local machine.
+For more extensive analysis or if you prefer to use MixTCRpred offline, it is recommended to install it on your local machine.
 
-## Install TCRpred 
+## Install MixTCRpred 
 
-1. Clone the GitHub repository and move to the TCRpred directory
+1. Clone the GitHub repository and move to the MixTCRpred directory
 ```bash
-git clone https://github.com/GfellerLab/TCRpred 
-cd TCRpred
+git clone https://github.com/GfellerLab/MixTCRpred 
+cd MixTCRpred
 ```
 
 2. (Recommended) Create a virtual environment and install the required packages
 ```bash
 # For Unix/Mac OS users
-python -m venv TCRpred_venv  
-source TCRpred_venv/bin/activate  # to activate the virtual environment (TCRpred_venv)
+python -m venv MixTCRpred_venv  
+source MixTCRpred_venv/bin/activate  # to activate the virtual environment (MixTCRpred_venv)
 pip install --upgrade pip
 pip install -r requirements.txt  # to install the packages
 
@@ -32,96 +32,99 @@ pip install -r requirements.txt  # to install the packages
 
 3. To test your installation, run the following command:
 ```bash
-python TCRpred.py --help
+python MixTCRpred.py --help
 ```
 ![](help_output.png)  
 
 or 
 
 ```bash
-python TCRpred.py --list_model
+python MixTCRpred.py --list_models
 ```
 
-which will show all the available TCRpred models.
+which will show all currently available MixTCRpred models.
 ![](list_model_output.png)
 
 4. Finally run 
 
 ```bash
-python TCRpred.py --model A0201_GILGFVFTL --input ./test/test.csv --output ./test/output.csv 
+python MixTCRpred.py --model A0201_GILGFVFTL --input ./test/test.csv --output ./test/output.csv 
 ```
 
-to predict which TCRs in the ./test/test.csv file are more likely to binding to the HLA-A\*02:01,GILGFVFTL epitope.
+to predict which TCRs in the ./test/test.csv file are more likely to target the HLA-A\*02:01,GILGFVFTL epitope.
 
 
 
-5. (Optional) To run TCRpred from anywhere on the computer, open TCRpred.py with your favourite editor and specify the full path to the pretrained models folder:
+5. (Optional) To run MixTCRpred from anywhere on the computer, open MixTCRpred.py with your favourite editor and specify the full path to the pretrained models folder:
 ```bash
 #change
 path_pretrained_models = './pretrained_models'
 #to 
-path_pretrained_models = '/home/[...]/TCRpred/pretrained_models'
+path_pretrained_models = '/home/[...]/MixTCRpred/pretrained_models'
 ```
-Next make an alias to the TCRpred.py file using the python version of the virtual enviroment:
+Next make an alias to the MixTCRpred.py file using the python version of the virtual enviroment:
 ```bash
 # For Unix/Mac OS users
-alias TCRpred='/home/[...]/TCRpred/TCRpred_venv/bin/python /home/[...]/TCRpred/TCRpred.py'
+alias MixTCRpred='/home/[...]/MixTCRpred/MixTCRpred_venv/bin/python /home/[...]/MixTCRpred/MixTCRpred.py'
 # you can make this alias permanent by adding it to your .bashrc file
 ```
 
 ## Usage
 
 ```bash
-source TCRpred_venv/bin/activate  # to activate the virtual environment (TCRpred_venv)
-python TCRpred.py --model [TCRpred_model_name] --input [input_TCR_file] --out [output_file]
+source MixTCRpred_venv/bin/activate  # to activate the virtual environment (MixTCRpred_venv)
+python MixTCRpred.py --model [MixTCRpred_model_name] --input [input_TCR_file] --out [output_file]
 ```
 
 Three arguments are required:
 
-``` --model``` or  ``` -m``` [TCRpred_model_name]. 
-To get the list of available models use ``` --help ```. 
-The format is HLAname_PeptideSequence (e.g. A0201_GILGFVFTL).
+``` --model``` or  ``` -m``` [MixTCRpred_model_name]. 
+The format is HLA_PeptideSequence (e.g. A0201_GILGFVFTL).
 
 ```--input``` or ```-i``` [input_TCR_file].
 csv file listing all the TCRs to test. See ./test/test.cvs for a reference input file. The columns order is not important.
 CDR3 alpha and beta should not be longer than 20 amino acids.
-Incomplete TCR entries are accepted, but models have lower predictive performance
+Incomplete TCR entries are accepted, but the models will have lower predictive performance
 
 ``` --output``` or ```-o``` [output_file].
-The name of the output file. It contains two more column than the input: the TCRpred binding score and the %rank.
+The name of the output file. It contains two extra columns than the input file: the MixTCRpred binding score and the %rank.
 
 
 Additional and optional arguments are:  
-```--list_model```. To visualize the 146 TCRpred models for which we can run predictions. Models with less than 50 training TCRs have low confidence  
-```--batch_size```. The default batch size is 1. If you have a large dataset of TCRs to test, increasing the batch_size can speed TCRpred up  
-```--download model_name```. To download a specific pretrained TCRpred model  
-```--download_all```. To download the 146 pretrained TCRpred models  
-```--download_high```. To download the 43 high-confidence pretrained TCRpred models 
-```-h``` or ```--help```. To print the help message.
+
+```--list_models```. To visualize the 146 MixTCRpred models for which we can run predictions. Models with less than 50 training TCRs have low confidence  
+
+```--batch_size```. The default batch size is 1. If you have a large dataset of TCRs to test, increasing the batch_size can speed MixTCRpred up  
+
+```--download model_name```. To download a specific pretrained MixTCRpred model  
+
+```--download_all```. To download the 146 pretrained MixTCRpred models  
+
+```--download_high```. To download the 43 high-confidence pretrained MixTCRpred models 
 
 
-## Download TCRpred pretrained models
+## Download MixTCRpred pretrained models
 
-In the GitHub repository we include only two TCRpred models (A0201_GILGFVFT and A0201_ELAGIGILTV). 
-You can download the pretrained TCRpred models from our [Zenodo dataset](https://doi.org/10.5281/zenodo.7930623)
+In the GitHub repository we include only two MixTCRpred models (A0201_GILGFVFT and A0201_ELAGIGILTV). 
+You can download the pretrained MixTCRpred models from our [Zenodo dataset](https://doi.org/10.5281/zenodo.7930623)
 
 To download a specific pretrained model (e.g. A0201_NLVPMVATV) run:
 ```bash
-python TCRpred.py --download A0201_NLVPMVATV 
+python MixTCRpred.py --download A0201_NLVPMVATV 
 ```
 
-To download all the 146 pretrained TCRpred models run:
+To download all the 146 pretrained MixTCRpred models run:
 ```bash
-python TCRpred.py --download
+python MixTCRpred.py --download_all
 ```
 
 To download the high-confidence 43 models (more than 50 training TCRs) run:
 ```bash
-python TCRpred.py --download_high
+python MixTCRpred.py --download_high
 ```
 
 ## Contact information
 
-For scientific questions, please contact [Giancarlo Croce](mailto:giancarlo.croce@unil.ch?subject=[GitHub]%20TCRpred%20) or [David Gfeller](mailto:david.gfeller@unil.ch?subject=[GitHub]%20TCRpred%20)
+For scientific questions, please contact [Giancarlo Croce](mailto:giancarlo.croce@unil.ch?subject=[GitHub]%20MixTCRpred%20) or [David Gfeller](mailto:david.gfeller@unil.ch?subject=[GitHub]%20MixTCRpred%20)
 
-For license-related questions, please contact [Nadette Bulgin](mailto:nbulgin@lcr.org?subject=[GitHub]%20TCRpred%20).
+For license-related questions, please contact [Nadette Bulgin](mailto:nbulgin@lcr.org?subject=[GitHub]%20MixTCRpred%20).
